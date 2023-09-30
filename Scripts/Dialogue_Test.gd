e8e3ed4dd5154871f5148783cbf7870e264a01fc
jakobwismar <98495236+jakobwismar@users.noreply.github.com>
2023-09-30 15:26:21 -0400

    dialog

--- Scripts/Dialogue_Test.gd
+++ Scripts/Dialogue_Test.gd
@@ -6,7 +6,11 @@ func _ready():
 	var new_dialog = Dialogic.start('TestSequence', false)
 	add_child(new_dialog)
 	
-	# TODO: connect signals when dialogue ends, or at certain points etc.
+	# TODO connect signals when dialogue ends
+	# new_dialog.connect("timeline_end", self, 'after_dialog')
+	
+func after_dialog(timeline_name):
+	print ('RESUME THE GAME?!')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
