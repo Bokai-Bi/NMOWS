afb33e10f1eec00645731fb9cf1ee20280c13dd8
jakobwismar <98495236+jakobwismar@users.noreply.github.com>
2023-09-30 15:19:58 -0400

    more dialogue (player choices)

--- Scripts/Dialogue_Test.gd
+++ Scripts/Dialogue_Test.gd
@@ -5,6 +5,8 @@ extends Node
 func _ready():
 	var new_dialog = Dialogic.start('TestSequence', false)
 	add_child(new_dialog)
+	
+	# TODO: connect signals when dialogue ends, or at certain points etc.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
