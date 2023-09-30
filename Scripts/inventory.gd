e9cae092214126c86febcde3582af7dc67287806
Luke Colburn <lukecolburn8@gmail.com>
2023-09-30 15:59:57 -0400

    Added inventory node to player with simple inventory script
    Currently only has a function to add items to the inventory.

---
+++ Scripts/inventory.gd
@@ -0,0 +1,19 @@
+extends Node
+
+# Dictionary to track how many of each item the player has
+# key is name of item as string
+# value is number of item held by the player
+var items = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# Add item specified by item_name to inventory
+func add_item(item_name):
+	# Add 1 to number of item held, or set to 1 if item isn't yet held
+	items[item_name] = 1 + items[item_name] if items.has(item_name) else 1
