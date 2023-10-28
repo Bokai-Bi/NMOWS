35cc565f556564ddf89bc01fcd03f67955c19bbc
“Timfon” <timothy_fong@brown.edu>
2023-10-28 14:38:53 -0400

    fix: player movement more responsive

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,30 +1,29 @@
 extends RigidBody2D
-
 @export var speed = 180
 
-
+var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await get_tree().process_frame
 	get_tree().call_group("killer", "set_player", self)
 
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var direction = Vector2.ZERO
-
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_down"):
-		direction.y += 1
-	if Input.is_action_pressed("move_up"):
-		direction.y -= 1
-
-	direction = direction.normalized() * speed * delta
-	move_and_collide(direction)
+	get_input()
+	velocity = move_and_collide(velocity)
 	
 	if Input.is_action_just_pressed("interact"):
 			$Collector.collect_items_if_any()
