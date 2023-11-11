95b6e29c68cd4806622e75d143d49d5d085d89cc
Jason Lin <jason_lin@brown.edu>
2023-11-11 16:34:18 -0500

    Fixed Camera, Fixed Viewing

--- Scripts/next_Scene.gd
+++ Scripts/next_Scene.gd
@@ -14,9 +14,9 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.name == "GridPlayer":
 		print("Go to Next Level")
-		get_tree().change_scene_to_file("res://dating_sim_scene.tscn")
+		get_tree().change_scene_to_file("res://DialogueReal/Scenes/level_1_dialogue_scene.tscn")
 
 #func _on_body_entered(body):
 	if body.name == "GridPlayer":
 		print("Go to Next Level")
-		get_tree().change_scene_to_file("res://dating_sim_scene.tscn")
+		get_tree().change_scene_to_file("res://DialogueReal/Scenes/level_1_dialogue_scene.tscn")
