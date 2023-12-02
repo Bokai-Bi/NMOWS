0c18d1d606cbfcacd789ebf872fb1c5b3393a7a4
Bokai-Bi <87922022+Bokai-Bi@users.noreply.github.com>
2023-12-02 01:24:05 -0500

    Implemented all hiding animation

--- ObjectScenes/hideable_box.gd
+++ ObjectScenes/hideable_box.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 var animationPlayer
 
+@export var play3Frame = false
+
 func _ready():
 	animationPlayer = $AnimationPlayer
 	
 func hide_animation():
-	animationPlayer.play("test")
+	if play3Frame:
+		animationPlayer.play("test_3frame")
+	else:
+		animationPlayer.play("test")
--- Scripts/next_Scene.gd
+++ Scripts/next_Scene.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-
+@export var target_scene = ""
 
 func _on_body_entered(body):
 	if body.name == "GridPlayer":
 		print("Go to Next Level")
-		get_tree().change_scene_to_file("res://DialogueReal/Scenes/level_1_dialogue_scene.tscn")
+		#get_tree().change_scene_to_file("res://DialogueReal/Scenes/level_1_dialogue_scene.tscn")
+		get_tree().change_scene_to_file(target_scene)
